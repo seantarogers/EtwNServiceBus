819836c739d91bb903f321e0cd183dbafed72f3f
seantarogers <seantarogers@yahoo.co.uk>
2016-11-12 13:15:09 +0000

    Added easy logger to benchmarking

---
+++ PerformanceBenchmarking/EasyLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace PerformanceBenchmarking
+{
+    using Easy.Logger;
+
+    public static class EasyLogger
+    {
+        public static void InitializeTypeReference()
+        {
+            // need to do this to statically  
+            // because we only reference the easy.logger type in the app.configuration configuration
+            // ReSharper disable once UnusedVariable
+            var easyLogger = Log4NetService.Instance.GetLogger(typeof(Program));
+        }
+    }
+}
--- PerformanceBenchmarking/Program.cs
+++ PerformanceBenchmarking/Program.cs
@@ -8,17 +8,33 @@ using PerformanceBenchmarking.EventSources;
 
 namespace PerformanceBenchmarking
 {
+    using Easy.Logger;
+
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Comparing Log4Net against ETW over 10 seconds...");
+            Console.WriteLine("Comparing Log4Net and EasyLogger against ETW over 10 seconds...");
+
+            EasyLogger.InitializeTypeReference();
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
 
             //1. Test push traces to log4net to rolling file appender for 10 seconds
-            Task.Run(() => RunLog4NetTest());
+            Task.Run(() => RunStandardLog4NetTest());
 
-            //1. Test push traces to ETW for 10 seconds
+            //2. Test push traces to easy logger to rolling file appender for 10 seconds
+            Task.Run(() => RunEasyLoggerFastLog4NetLoggerTest());
+
+            //3. Test push traces to ETW for 10 seconds
             Task.Run(() => RunEtwTest());
+
+            Console.WriteLine("");
+            Console.WriteLine("(Once the results are reported, press any key to see number of garbage collections...)");
+            Console.ReadLine();
+            
+            Console.WriteLine("GC Gen 0: {0}", GC.CollectionCount(0));
+            Console.WriteLine("GC Gen 1: {0}", GC.CollectionCount(1));
+            Console.WriteLine("GC Gen 2: {0}", GC.CollectionCount(2));
 
             Console.ReadLine();
 
@@ -40,17 +56,13 @@ namespace PerformanceBenchmarking
 
             Console.WriteLine("");
             Console.WriteLine("========================");
-            Console.WriteLine("Number of ETW traces written to OS: {0:n0}, Time Taken: {1}", traceNumber, sw.Elapsed);
-            Console.WriteLine("ETW GC Gen 0: {0}", GC.CollectionCount(0));
-            Console.WriteLine("ETW GC Gen 1: {0}", GC.CollectionCount(1));
-            Console.WriteLine("ETW GC Gen 2: {0}", GC.CollectionCount(2));
+            Console.WriteLine("ETW: {0:n0} traces written to OS", traceNumber);
             Console.WriteLine("========================");
             Console.WriteLine("");
         }
 
-        private static void RunLog4NetTest()
+        private static void RunStandardLog4NetTest()
         {
-            XmlConfigurator.ConfigureAndWatch(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
             var log4NetLogger = LogManager.GetLogger(typeof(Program));
 
             var duration = TimeSpan.FromSeconds(10);
@@ -65,10 +77,27 @@ namespace PerformanceBenchmarking
 
             Console.WriteLine("");
             Console.WriteLine("========================");
-            Console.WriteLine("Number of Log4net traces written to logfile: {0:n0}, Time Taken: {1}", traceNumber, sw.Elapsed);
-            Console.WriteLine("Log4net GC Gen 0: {0}", GC.CollectionCount(0));
-            Console.WriteLine("Log4net GC Gen 1: {0}", GC.CollectionCount(1));
-            Console.WriteLine("Log4net GC Gen 2: {0}", GC.CollectionCount(2));
+            Console.WriteLine("Standard Log4Net: {0:n0} traces written to log file" , traceNumber);
+            Console.WriteLine("========================");
+            Console.WriteLine("");
+        }
+
+        private static void RunEasyLoggerFastLog4NetLoggerTest()
+        {
+            var easyLogger = Log4NetService.Instance.GetLogger("EasyLogger");
+            var duration = TimeSpan.FromSeconds(10);
+
+            var sw = Stopwatch.StartNew();
+            long traceNumber = 0;
+            while (sw.Elapsed < duration)
+            {
+                traceNumber++;
+                easyLogger.DebugFormat("easyLogger debug statement. Number of traces: {0}", traceNumber);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("========================");
+            Console.WriteLine("Easy Logger: {0:n0} traces written to log file", traceNumber);
             Console.WriteLine("========================");
             Console.WriteLine("");
         }
